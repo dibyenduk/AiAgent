650ae23452336e9a99b6b44e36055048acf29d4d
Dibyendu Kankalia <dibyendu.kankalia@aveva.com>
2025-11-28 11:42:04 -0600

    This is a first implementation for AI Agent - Sync and Streaming

---
+++ AgentService/Program.cs
@@ -0,0 +1,21 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using Microsoft.Agents.AI;
+using OpenAI;
+
+var endpoint = new Uri("https://constructionbiddingmode-resource.cognitiveservices.azure.com/");
+var deploymentName = "gpt-4.1";
+var apiKey = "<API Token>";
+
+var azureClient = new AzureOpenAIClient(endpoint, new AzureKeyCredential(apiKey));
+
+
+var aiAgent = azureClient.GetChatClient(deploymentName).CreateAIAgent();
+
+//var response = await aiAgent.RunAsync("What is the capital of France ?");
+//System.Console.WriteLine(response);
+
+await foreach(AgentRunResponseUpdate update in aiAgent.RunStreamingAsync("How to make soup ?"))
+{
+    System.Console.Write(update);
+}
