58a612229c9f4cfbe25436f1f5fdc108d11602c7
Dibyendu Kankalia <dibyendu.kankalia@aveva.com>
2025-11-28 11:43:47 -0600

    Update program.cs

--- AgentService/Program.cs
+++ AgentService/Program.cs
@@ -3,7 +3,7 @@ using Azure.AI.OpenAI;
 using Microsoft.Agents.AI;
 using OpenAI;
 
-var endpoint = new Uri("https://constructionbiddingmode-resource.cognitiveservices.azure.com/");
+var endpoint = new Uri("<url>");
 var deploymentName = "gpt-4.1";
 var apiKey = "<API Token>";
 
