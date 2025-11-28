4f3f21c029f5f47fc432d6d4fd08f9acfa573d3a
Dibyendu Kankalia <dibyendu.kankalia@aveva.com>
2025-11-28 12:29:48 -0600

    Agent with tools

--- AgentService/Program.cs
+++ AgentService/Program.cs
@@ -1,25 +1,26 @@
-﻿using Azure;
+﻿using AgentService;
+using Azure;
 using Azure.AI.OpenAI;
 using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
 using OpenAI;
 
-var endpoint = new Uri("<Url>");
+var endpoint = new Uri("Url");
 var deploymentName = "gpt-4.1";
 var apiKey = "<API Key>";
 
 var azureClient = new AzureOpenAIClient(endpoint, new AzureKeyCredential(apiKey));
 
-
+//Simple agent run
 var aiAgent = azureClient.GetChatClient(deploymentName).CreateAIAgent();
-
 var response = await aiAgent.RunAsync("What is the capital of France ?");
 Console.WriteLine(Environment.NewLine);
 System.Console.WriteLine(response);
 Console.WriteLine("Input Token: " + response?.Usage?.InputTokenCount + " tokens" + "Output Token: " + response?.Usage?.OutputTokenCount + " tokens");
 
-
+// Streaming agent run
 var updates = new List<AgentRunResponseUpdate>();
-await foreach(AgentRunResponseUpdate update in aiAgent.RunStreamingAsync("How to make soup ?"))
+await foreach (AgentRunResponseUpdate update in aiAgent.RunStreamingAsync("How to make soup ?"))
 {
     updates.Add(update);
     System.Console.Write(update);
@@ -29,3 +30,24 @@ Console.WriteLine(Environment.NewLine);
 response = updates.ToAgentRunResponse();
 Console.WriteLine("Input Token count: " + response?.Usage?.InputTokenCount);
 Console.WriteLine("Output Token count: " + response?.Usage?.OutputTokenCount);
+
+// Agent with tools
+var agent = azureClient.GetChatClient(deploymentName).CreateAIAgent(
+    instructions: "You are a time expert",
+    tools: new List<AITool>()
+    {
+        AIFunctionFactory.Create(Tools.GetCurrentDateAndTime, "current_date_and_time"),
+        AIFunctionFactory.Create(Tools.GetCurrentTimeZone, "current_timezone")
+    });
+
+AgentThread thread = agent.GetNewThread();
+
+while(true)
+{
+    Console.Write("> ");
+    string? userInput = Console.ReadLine();
+    ChatMessage message = new ChatMessage(ChatRole.User, userInput);
+    var timeResponse = await agent.RunAsync(message, thread);
+    Console.WriteLine(timeResponse);
+    Console.WriteLine(Environment.NewLine);
+}
---
+++ AgentService/Tools.cs
@@ -0,0 +1,37 @@
+﻿using Azure.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgentService
+{
+    public static class Tools
+    {
+        // [Description("Gets the current date and time based on the specified TimeType.")]
+        public static DateTime GetCurrentDateAndTime(TimeType timeType)
+        {
+            return timeType switch
+                {
+                TimeType.Local => DateTime.Now,
+                TimeType.Utc => DateTime.UtcNow,
+                _ => throw new ArgumentOutOfRangeException(nameof(timeType), timeType, null)
+            };
+        }
+
+        // [Description("Gets the current time zone of the system.")]
+        public static string GetCurrentTimeZone()
+        {
+            return TimeZoneInfo.Local.StandardName;
+        }
+
+    }
+
+    public enum TimeType
+    {
+        Local,
+        Utc
+    }
+}
